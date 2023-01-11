9d67c26d3435659500bfbf32a3ac3955c9463bb8
Connor-Vickers <connorvic97@gmail.com>
2023-01-10 21:26:23 -0500

    added dose change logic and graphing

--- src/App.vue
+++ src/App.vue
@@ -18,6 +18,20 @@
         <label>Starting Dose:</label>
         <input v-model="medication.starting_dose" type="number" />
       </div>
+      <div v-for="doseChange in medication.doseChanges">
+        <div>
+          <label>Dose changed to:</label>
+          <input v-model="doseChange.dose" type="number" />
+        </div>
+        <div>
+          <label>on:</label>
+          <input v-model="doseChange.date" type="date" />
+        </div>
+      </div>
+
+      <button type="button" @click="addDoseChange(medication)">
+        Add Dose Change
+      </button>
       <div>
         <label>Currently Taking:</label>
         <input v-model="medication.currently_taking" type="checkbox" />
@@ -56,23 +70,34 @@ export default {
         starting_dose: 0,
         currently_taking: true,
         end_date: new Date(),
-        doses: {},
+        doseChanges: [],
       });
     },
+    addDoseChange(medication) {
+      console.log(medication);
+      medication.doseChanges.push({ date: new Date(), dose: 0 });
+    },
     update_chart() {
+      var dose = this.medications[0].starting_dose;
       let data = [
         { x: this.medications[0].start_date, y: 0 },
         {
           x: this.medications[0].start_date,
-          y: this.medications[0].starting_dose,
+          y: dose,
         },
       ];
+      for (var doseChange of this.medications[0].doseChanges) {
+        console.log(doseChange);
+        data.push({ x: doseChange.date, y: dose });
+        dose = doseChange.dose;
+        data.push({ x: doseChange.date, y: doseChange.dose });
+      }
       if (this.medications[0].currently_taking) {
-        data.push({ x: new Date(), y: this.medications[0].starting_dose });
+        data.push({ x: new Date(), y: dose });
       } else {
         data.push({
           x: this.medications[0].end_date,
-          y: this.medications[0].starting_dose,
+          y: dose,
         });
         data.push({ x: this.medications[0].end_date, y: 0 });
       }
